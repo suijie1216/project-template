dabf250ffd0c12ca7cefeaf971e7e9507b5477a3
suijie <suijieeven@didichuxing.com>
2018-06-09 11:11:19 +0800

    opti

---
+++ template/src/main/resources/archetype-resources/__artifactId__-api/src/main/java/__packageInPathFormat__/api/web/config/FilterConfig.java
@@ -0,0 +1,49 @@
+package ${package}.api.web.config;
+
+import ${package}.api.web.filter.QPSFilter
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.litchi.profile.qps.QPSCounterWorker;
+import org.springframework.litchi.profile.qps.QPSListener;
+import org.springframework.litchi.profile.qps.QPSNode;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author suijie
+ */
+@Configuration
+public class FilterConfig extends WebMvcConfigurerAdapter {
+    private final static Logger logger = LoggerFactory.getLogger("trace-log");
+    @Resource
+    private QPSFilter qpsFilter;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(qpsFilter).addPathPatterns("/api/**");
+    }
+
+    @Bean
+    public QPSCounterWorker buildQPSCounterWorker() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        QPSListener listener = new QPSListener() {
+            @Override
+            public void event(QPSNode node) {
+                String result = String.format("api:%s,time:%s,qps:%s,rt:%s",
+                        node.getPath(),
+                        format.format(new Date(node.getTime())),
+                        node.getQps(),
+                        node.getRt().longValue() / node.getQps().intValue());
+                logger.info(result);
+            }
+        };
+        return new QPSCounterWorker(listener);
+    }
+
+}
---
+++ template/src/main/resources/archetype-resources/__artifactId__-api/src/main/java/__packageInPathFormat__/api/web/filter/QPSFilter.java
@@ -0,0 +1,52 @@
+package ${package}.api.web.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.litchi.profile.qps.QPSCounterWorker;
+import org.springframework.litchi.profile.trace.Trace;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author suijie
+ */
+@Service
+public class QPSFilter implements HandlerInterceptor {
+    private final static Logger logger = LoggerFactory.getLogger("trace-log");
+
+    @Resource
+    private QPSCounterWorker qpsCounterWorker;
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) {
+        String path = httpServletRequest.getServletPath();
+        Trace.traceIn(path);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        try {
+            String path = httpServletRequest.getServletPath();
+            Trace.traceOut(path);
+            long cost = Trace.getCost(path);
+            qpsCounterWorker.increment(path, cost);
+            if (cost > 200L) {
+                logger.info(Trace.printTrace());
+            }
+        } finally {
+            Trace.reset();
+        }
+
+    }
+}
